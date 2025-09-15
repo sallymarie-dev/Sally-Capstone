21c4991282a82ee4a9394aec60ebbed3a3c5e851
Sally <causeysally@gmail.com>
2025-09-15 01:02:56 -0500

    broken code added variables

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ document
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        console.log(data === 1);
       });
     if (data.status === 1) {
       var item = data.product;
@@ -48,23 +48,25 @@ document
       }
 
       // Get nutrients safely
+      var nutrients = item.nutriments;
+      // Some items might not have all fields
       var nutrients = item.nutriments || {};
       var calories = "N/A";
       var protein = "N/A";
       var carbs = "N/A";
       var fat = "N/A";
-
-      if (nutrients["energy-kcal_100g"]) {
-        calories = nutrients["energy-kcal_100g"] + " kcal";
+      /// display info
+      if (nutrients) {
+        calories = nutrients + " kcal";
       }
-      if (nutrients.proteins_100g) {
-        protein = nutrients.proteins_100g + " g";
+      if (nutrients.proteins) {
+        protein = nutrients.proteins + " g";
       }
-      if (nutrients.carbohydrates_100g) {
-        carbs = nutrients.carbohydrates_100g + " g";
+      if (nutrients.carbohydrates) {
+        carbs = nutrients.carbohydrates + " g";
       }
-      if (nutrients.fat_100g) {
-        fat = nutrients.fat_100g + " g";
+      if (nutrients.fat) {
+        fat = nutrients.fat + " g";
       }
 
       setText(
@@ -85,33 +87,11 @@ document
       setText("error-message", "Product not found for that barcode.");
     }
   })
+  //if barcode is wrong or invalid
   .catch((error) => {
     console.error("Fetch error:", error);
     setText("error-message", "Error fetching data. Please try again.");
   });
-
-var botReply = "";
-// botReply=response.choices[0].message.content
-var userPrompt = "";
-document.getElementById("Ai-btn").addEventListener("click", function () {
-  userPrompt = getValue("Ai-input").trim();
-});
-
-async function query(data) {
-  const response = await fetch(
-    "https://router.huggingface.co/v1/chat/completions",
-    {
-      headers: {
-        Authorization: `Bearer ${HF_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify(data),
-    }
-  );
-  const result = await response.json();
-  return result;
-}
 
 query({
   messages: [
